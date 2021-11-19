f8d568ec0c124da759d09de17b9bfc20ef101797
PedroMP93 <pmaldona93@gmail.com>
2021-11-19 08:11:46 -0500

    primera version

---
+++ src.js
@@ -0,0 +1,13 @@
+const visitas = [];
+
+const insertarVisita = (fecha, finca, lote, labor, area, tecnico) =>{
+
+}
+
+const filtrarResultados = (key, value) =>{
+
+}
+
+module.exports.filtrarResultados = filtrarResultados;
+module.exports.insertarVisita = insertarVisita;
+module.exports.visitas = visitas;
