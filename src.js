be824e99075cf65ccf8d8ca13960090303fc597d
Luisa Pereira <lufe.pereira23@gmail.com>
2021-12-02 21:49:12 -0500

    Update src.js

--- src.js
+++ src.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+//const port = 5000;
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+global.technical_visit = require('./technical-visit.json');
 
 
-app.get('/', (req, res)=>{
-    res.send("Welcom to Express");
+app.get('/api/agricultural-inputs/technical_visit',
+    (req, res)=>{
+    //console.log(visitasAPI);
+
+    return res.json(technical_visit);
 });
 
-app.post('/', (req,res)=>{
+
+app.post('/api/agricultural-inputs/technical_visit', (req,res)=>{
+    const fecha = req.body.fecha; 
+    const finca = req.body.finca; 
+    const lote = req.body.lote; 
+    const labor = req.body.labor; 
+    const area = req.body.area; 
+    const tecnico = req.body.tecnico;
+
+    const visita = {
+        'fecha': fecha,
+        'finca': finca,
+        'lote': lote,
+        'labor': labor,
+        'area': area,
+        'tecnico': tecnico
+    }
+
+    technical_visit.push(visita);
+
+    return res.send(visita);
 
 });
 
-
-app.listen(port,() => {
+/*app.listen(port,() => {
     console.log(`Started on PORT ${port}`);
-});
+});*/
 
 module.exports = app;
